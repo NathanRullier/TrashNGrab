1966389f301696e095c83e2f05e2c5e3f7ecc01e
Stéphane Burwash <sburwash@live.ca>
2019-01-19 15:16:25 -0500

    SB Dropdown menu created

--- Client/src/app/page-presentation/page-presentation.component.ts
+++ Client/src/app/page-presentation/page-presentation.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-presentation',
   templateUrl: './page-presentation.component.html',
-  styleUrls: ['./page-presentation.component.css']
+  styleUrls: ['./page-presentation.component.css'],
+
 })
 
 
 export class PagePresentationComponent implements OnInit {
 
+  clickMessage = '';
+  onClickMe(): void {
 
+    this.clickMessage = 'I learnt how to click';
+  }
   constructor() { }
 
   ngOnInit() {
