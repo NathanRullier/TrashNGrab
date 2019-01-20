7f56a82d5dad9e6eecf847872f5f77a64a2acf8a
Mathieu Marchand <mathieu.marchand@polymtl.ca>
2019-01-19 21:11:48 -0500

    add email service

--- server/app/email/emailservice.ts
+++ server/app/email/emailservice.ts
@@ -1,1 +1,33 @@
-import {} from "nodemailer";
+import {createTransport} from "nodemailer";
+
+
+export class EmailService {
+
+    public sendGreeting(emailAddress: string){
+        let transporter = createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'trashngrab@gmail.com',
+                pass: 'hackatown2019'
+            }
+        });
+
+        let mailOptions = {
+            from: "trashngrab@gmail.com", // sender address
+            to: emailAddress, // list of receivers
+            subject: "subscription", // Subject line
+            text: "H! Welcome to trashNGrab", // plain text body
+            html: '<b>NodeJS Email Tutorial</b>' // html body
+        };
+
+        transporter.sendMail(mailOptions, ((err, info) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        }))
+
+    }
+}
