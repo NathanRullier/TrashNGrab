3a21fe0ffb8daa93a852186986feb70ee2e0effe
Unknown <nathan.rullier@gmail.com>
2019-01-19 21:54:13 -0400

    wtf is happening

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagePresentationComponent } from './page-presentation/page-presentation.component';
+import { MapComponent } from './map/map.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagePresentationComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyD4w4AH-yl2Uc5t1ONtdPjMxlLVI8ImyxI'
+    }),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
