3aba90e0e8038d1538f81ba845a880c0970a7b13
Stéphane Burwash <sburwash@live.ca>
2019-01-20 04:39:18 -0500

    Server Signup works

--- Client/src/app/login-supplier/login-supplier.component.ts
+++ Client/src/app/login-supplier/login-supplier.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Http } from '@angular/http';
+
+
+
 
 @Component({
   selector: 'app-login-supplier',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-supplier.component.css']
 })
 export class LoginSupplierComponent implements OnInit {
+  
+  
+  firstName = "";
+  lastName ="";
+  emailAddress ="";
+  Address ="";
+  Password ="";
+  ZipCode = "";
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() {
   }
+  onSignUp(): void
+  {
+    this.firstName = (document.getElementById("firstName") as HTMLInputElement).value;
+    this.lastName = (document.getElementById("lastName") as HTMLInputElement).value;
+    this.Password = (document.getElementById("passwd") as HTMLInputElement).value;
+    this.emailAddress = (document.getElementById("email") as HTMLInputElement).value;
+    this.Address = (document.getElementById("Address") as HTMLInputElement).value;
+    this.ZipCode = (document.getElementById("Zip-Code") as HTMLInputElement).value;
 
+    // let post_1 = 
+    // console.log(post_1);
+    this.http.post('http://localhost:3000/signup/producer', {
+
+      "name": this.firstName + this.lastName,
+      "password": this.Password,
+      "email": this.emailAddress,
+      "adress": this.Address,
+      "zipcode": this.ZipCode
+
+    }).subscribe(data=>{console.log(data)});
+
+  }
 }
